134e44010e4b0c47c9b3071e2dd7e33960735e49
John hinrich <johnhinrichggalindo@gmail.com>
2021-07-05 18:07:21 +0800

    refactored some of the codes

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let Website = {
+const App = {
   //object properties
 
   category : document.getElementById("anime-page"),
@@ -21,20 +21,12 @@ let Website = {
 
   // method 1
   
-  renderImages(){
+  init(){
 
   const { category, modalBox, body, ModalImage, photographer, photoLink } = this;
     
     let initialSearch = new URLSearchParams(window.location.search).get("search");
     const imageData = {
-      // thumbnails : [],
-      // mobile : [],
-      // mobileThumnnails : [],
-      // hd : [],
-      // large2x : [],
-      // large : [],
-      // portraits : [],
-      // landscapes : [],
       width: [],
       height: [],
       photographer : [],
@@ -47,7 +39,9 @@ let Website = {
     let totalRendered = 0;
     const banner = document.querySelector('.banner')
     async function updateValue(url) {
-      console.log(url)
+
+      if(banner.innerHTML.includes('No more images to load')) return;
+      //console.log(url)
       const searchInput = document.getElementById("search-input");
       
       const res = await fetch(url,{
@@ -99,161 +93,163 @@ let Website = {
     let called = 0;
     const renderUpdate = async data =>{
       let res = await data
-      console.log(called++)
+      if(res == undefined || null) return;
+
       //console.log("this is the res inside renderUpdate", res)
       let images = res.photos
-      if(images.length > 0){
+      if(images.length <= 0) return;
         
-        //console.log(images)
+      //console.log(images)
 
-        document.getElementsByTagName('title')[0].innerHTML = ` ${res.total_results} results for ${initialSearch}`
+      document.getElementsByTagName('title')[0].innerHTML = ` ${res.total_results} results for ${initialSearch}`
 
-        images.forEach(image => {
-          
-         
+      images.forEach(image => {
+        
+        
 
-          const newImage = document.createElement('img')
-          newImage.style.width = '0px';
+        const newImage = document.createElement('img')
+        newImage.style.width = '0px';
 
-          // window.innerWidth > 700 ? newImage.src = image.src.portrait.replace('&h=1200&w=800','&h=20&w=10') : 
-          // newImage.src = image.src.tiny.replace('&h=200&w=280','&h=10&w=20');
-          
-          let imageTitle = image.url.substr(29)
-          
-          while(imageTitle.includes('-') || imageTitle.includes('/')){
-            imageTitle= imageTitle.replace('-',' ')
-            imageTitle= imageTitle.replace(/[0-9]/g, '')
-            imageTitle= imageTitle.replace('/', '')
-          }
+        // window.innerWidth > 700 ? newImage.src = image.src.portrait.replace('&h=1200&w=800','&h=20&w=10') : 
+        // newImage.src = image.src.tiny.replace('&h=200&w=280','&h=10&w=20');
+        
+        let imageTitle = image.url.substr(29)
+        
+        while(imageTitle.includes('-') || imageTitle.includes('/')){
+          imageTitle= imageTitle.replace('-',' ')
+          imageTitle= imageTitle.replace(/[0-9]/g, '')
+          imageTitle= imageTitle.replace('/', '')
+        }
 
-          // newImage.height = image.height
-          // newImage.width = image.width
-          newImage.alt = imageTitle
-          newImage.className = 'image'
+        // newImage.height = image.height
+        // newImage.width = image.width
+        newImage.alt = imageTitle
+        newImage.className = 'image'
 
-          const newDiv = document.createElement('Div')
-          
-          newDiv.className = 'image-container'
+        const newDiv = document.createElement('Div')
+        
+        newDiv.className = 'image-container'
 
-          if(image.width > image.height){
-            newDiv.classList.add('horizontal')
-          }
-          else if(image.width < image.height){
-            newDiv.classList.add('vertical')
-          }
-          else{
-            newDiv.classList.add('big')
-          }
+        if(image.width > image.height){
+          newDiv.classList.add('horizontal')
+        }
+        else if(image.width < image.height){
+          newDiv.classList.add('vertical')
+        }
+        else{
+          newDiv.classList.add('big')
+        }
 
-          newDiv.style.backgroundColor = image.avg_color;
+        newDiv.style.backgroundColor = image.avg_color;
 
 
-          const newOverlayContainer = document.createElement('Div')
-          const newOverlay = document.createElement('h3')
-          newOverlayContainer.className = 'image-overlay'
-          newOverlay.innerHTML = imageTitle
-          
-          newDiv.append(newImage)
-          newOverlayContainer.append(newOverlay)
-          newDiv.append(newOverlayContainer)
-          category.append(newDiv)
+        const newOverlayContainer = document.createElement('Div')
+        const newOverlay = document.createElement('h3')
+        newOverlayContainer.className = 'image-overlay'
+        newOverlay.innerHTML = imageTitle
+        
+        newDiv.append(newImage)
+        newOverlayContainer.append(newOverlay)
+        newDiv.append(newOverlayContainer)
+        category.append(newDiv)
 
-          imageData.photographer.push(image.photographer)
-          imageData.url.push(image.url)
-          imageData.photographer_url.push(image.photographer_url)
+        imageData.photographer.push(image.photographer)
+        imageData.url.push(image.url)
+        imageData.photographer_url.push(image.photographer_url)
 
-          imageData.src.push(image.src)
-          imageData.height.push(image.height)
-          imageData.width.push(image.width)
+        imageData.src.push(image.src)
+        imageData.height.push(image.height)
+        imageData.width.push(image.width)
+
+      });
+
+    
+      // an array of images that are present in anime-page
+      categoryArray = Array.from( document.querySelectorAll("#anime-page .image") );
+      //console.log(categoryArray)
+
+      const head =  document.getElementsByTagName('head')[0]
+      const imgOptions = {};
+      const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) return;
+        const img = entry.target;  
+        img.src = imageData.src[categoryArray.indexOf(img)].medium
+        console.log(imageData.height[categoryArray.indexOf(img)] , img)
+ 
+        const linkImagePreloader = document.createElement('link')
+        linkImagePreloader.rel = 'preload'
+        
+
+        linkImagePreloader.href = imageData.src[categoryArray.indexOf(img)].medium
+
+        linkImagePreloader.as = "image"         
+        head.append(linkImagePreloader) 
+        img.style.width = '100%';
+
+      });
+      }, imgOptions);
+
+      let requestNow = 0;
+      categoryArray.forEach((img) => {
+        imgObserver.observe(img);
+        requestNow++
+      });
+
+      // const banner = document.createElement('Div')
+      // banner.classList.add('banner')
+      // banner.innerHTML = 'Loading more images...'
+      // const footer = document.getElementsByTagName('footer')[0]
+
+      //footer.insertBefore(banner, footer.firstChild)
+      
+      const bannerOptions = {}
+      const bannerObserver = new IntersectionObserver((entries, bannerObserver) => {
+
+        entries.forEach((entry) => {
+          if (!entry.isIntersecting) return;          
+          getData()
 
         });
+
+      },bannerOptions)
+
+      bannerObserver.observe(banner)
+      if('all images are loaded and banner is visible'){
+        'getdata'
+      } 
+
+      const imageOnClick = function() {
+        categoryArray.forEach(image =>{ 
+          image.addEventListener("click", function () {
+            if (modalBox.classList == "close") {
+              modalBox.classList.replace("close", "open");
+              body.classList.add("no-scroll");
+
+
+              if(window.innerWidth > 1900){
+                ModalImage.src = imageData.src[categoryArray.indexOf(this)].original;
+              }
+              else if(window.innerWidth > 1400){
+                ModalImage.src = imageData.src[categoryArray.indexOf(this)].large2x;
+              }else if(window.innerWidth > 700){
+                ModalImage.src = imageData.src[categoryArray.indexOf(this)].large;
+              }else if(window.innerWidth < 700){
+                ModalImage.src = imageData.src[categoryArray.indexOf(this)].medium;
+              }
+              
+              modalCurrentIndex = categoryArray.indexOf(this);
+              photoLink.href = imageData.url[categoryArray.indexOf(this)];
+              //console.log(imageData)
+              photographer.href = imageData.photographer_url[categoryArray.indexOf(this)];
+              photographer.innerHTML = imageData.photographer[categoryArray.indexOf(this)];
+            }
+          })
+        })
+      }
+      imageOnClick();
 
       
-        // an array of images that are present in anime-page
-        categoryArray = Array.from( document.querySelectorAll("#anime-page .image") );
-        //console.log(categoryArray)
-
-        const head =  document.getElementsByTagName('head')[0]
-        const imgOptions = {};
-        const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-        entries.forEach((entry) => {
-          if (!entry.isIntersecting) return;
-          const img = entry.target;  
-          img.src = imageData.src[categoryArray.indexOf(img)].medium
-          
-          const linkImagePreloader = document.createElement('link')
-          linkImagePreloader.rel = 'preload'
-          
-
-          linkImagePreloader.href = imageData.src[categoryArray.indexOf(img)].medium
-
-          linkImagePreloader.as = "image"         
-          head.append(linkImagePreloader) 
-          img.style.width = '100%';
-
-        });
-        }, imgOptions);
-
-        let requestNow = 0;
-        categoryArray.forEach((img) => {
-          imgObserver.observe(img);
-          requestNow++
-        });
-
-        // const banner = document.createElement('Div')
-        // banner.classList.add('banner')
-        // banner.innerHTML = 'Loading more images...'
-        // const footer = document.getElementsByTagName('footer')[0]
-
-        //footer.insertBefore(banner, footer.firstChild)
-        
-        const bannerOptions = {}
-        const bannerObserver = new IntersectionObserver((entries, bannerObserver) => {
-
-          entries.forEach((entry) => {
-            if (!entry.isIntersecting) return;          
-            getData()
-
-          });
-
-        },bannerOptions)
-
-        bannerObserver.observe(banner)
-        if('all images are loaded and banner is visible'){
-          'getdata'
-        } 
-
-        const imageOnClick = function() {
-          categoryArray.forEach(image =>{ 
-            image.addEventListener("click", function () {
-              if (modalBox.classList == "close") {
-                modalBox.classList.replace("close", "open");
-                body.classList.add("no-scroll");
-
-
-                if(window.innerWidth > 1900){
-                  ModalImage.src = imageData.src[categoryArray.indexOf(this)].original;
-                }
-                else if(window.innerWidth > 1400){
-                  ModalImage.src = imageData.src[categoryArray.indexOf(this)].large2x;
-                }else if(window.innerWidth > 700){
-                  ModalImage.src = imageData.src[categoryArray.indexOf(this)].large;
-                }else if(window.innerWidth < 700){
-                  ModalImage.src = imageData.src[categoryArray.indexOf(this)].medium;
-                }
-                
-                modalCurrentIndex = categoryArray.indexOf(this);
-                photoLink.href = imageData.url[categoryArray.indexOf(this)];
-                //console.log(imageData)
-                photographer.href = imageData.photographer_url[categoryArray.indexOf(this)];
-                photographer.innerHTML = imageData.photographer[categoryArray.indexOf(this)];
-              }
-            })
-          })
-        }
-        imageOnClick();
-
-      }
      
 
     }
@@ -293,10 +289,10 @@ let Website = {
     modalCloseButton.addEventListener("click", function () {
       modalBox.classList.replace("open", "close");
       body.classList.remove("no-scroll");
-      ModalImage.src = "";
-      photoLink.href = "";
-      photographer.innerHTML = "";
-      photographer.href = "";
+      // ModalImage.src = "";
+      // photoLink.href = "";
+      // photographer.innerHTML = "";
+      // photographer.href = "";
     });
 
 
@@ -308,10 +304,8 @@ let Website = {
     const getData = async () =>{
       
 
-      if (lastCalled >= (Date.now() - delay))
-        return;
-      
-
+      if (lastCalled >= (Date.now() - delay)) return;
+  
       //const nextData = await fetchNextPage(data,requestedPage)
       let res = updateValue(`https://api.pexels.com/v1/search?per_page=${totalImages}&page=${requestedPage}&query=${initialSearch}`)
       //console.log('this is the res inside getData',res)
@@ -329,4 +323,4 @@ let Website = {
 
 
 //invocation
-window.addEventListener("DOMContentLoaded", () => Website.renderImages());
+window.addEventListener("DOMContentLoaded", () => App.init());
